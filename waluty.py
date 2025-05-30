50d8701c37b931e4a0654e2361f6202b59f1c871
Jakub Mańko <121977820+jmanko1@users.noreply.github.com>
2025-05-30 19:07:20 +0200

    Added RSI, SMA and Bollinger Bands

---
+++ waluty.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+from datetime import datetime, timedelta
+
+ticker_symbol = 'EURPLN=X'
+
+end_date = datetime.now()
+start_date = end_date - timedelta(days=180)
+
+data = yf.download(
+    ticker_symbol,
+    start=start_date.strftime('%Y-%m-%d'),
+    end=end_date.strftime('%Y-%m-%d'),
+    interval='1h'
+)
+
+# Zapisz do pliku CSV
+csv_filename = f"{ticker_symbol.replace('=','')}_4h_11.csv"
+data.to_csv(csv_filename)
+
+print(f"Dane zapisano do pliku: {csv_filename}")
